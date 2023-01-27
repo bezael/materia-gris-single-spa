44d8d45bf2fcabd08ebc17b4467e0d9feb9e39fd
bezael <bezael@gmail.com>
2023-01-27 11:17:06 +0100

    feat(navbar): Add navbar app

---
+++ mf-store-navbar/src/components/navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+const Navbar = () => {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+      <div className='container-fluid'>
+        <a className='navbar-brand' href='/'>
+          Materia<span className='text-warning'>Store</span>
+        </a>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarNav'
+          aria-controls='navbarNav'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id='navbarNav'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <a className='nav-link active' aria-current='page' href='/products'>
+                Products
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ mf-store-navbar/src/root.component.tsx
@@ -0,0 +1,5 @@
+import Navbar from './components/navbar/Navbar';
+
+export default function Root() {
+  return <Navbar />;
+}
